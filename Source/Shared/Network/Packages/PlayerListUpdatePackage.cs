30682de8611dd440aa3ca671d970d630fd5844c2
Stefan Boronczyk <stefan.amin.boronczyk@googlemail.com>
2022-03-25 21:07:54 +0100

    Remove ignored files

--- Source/Shared/Network/Packages/PlayerListUpdatePackage.cs
+++
@@ -1,9 +0,0 @@
-namespace Shooter.Shared.Network.Packages
-{
-    public class PlayerListUpdatePackage
-    {
-        public PlayerUpdatePackage[] Players { get; set; }
-        public PlayerStatePackage[] PlayerStates { get; set; }
-        public uint WorldTick { get; set; }
-    }
-}
--- Source/Shared/Network/Packages/WorldNetPackage.cs
+++
@@ -1,8 +0,0 @@
-namespace Shooter.Shared.Network.Packages
-{
-    public class WorldNetPackage
-    {
-        public string WorldName { get; set; }
-        public uint WorldTick { get; set; }
-    }
-}
