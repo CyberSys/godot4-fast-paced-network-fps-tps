f4d8d83b611cd8f10738da21f6d87d9d1eac27a4
Stefan Boronczyk <stefan.amin.boronczyk@googlemail.com>
2022-08-18 22:01:19 +0200

    Delete editor.cmd

--- editor.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-:: --HAS ENDING BACKSLASH
-set batdir=%~dp0
-:: --MISSING ENDING BACKSLASH
-:: set batdir=%CD%
-pushd "%batdir%"
-%batdir%\Editor\windows\godot.windows.opt.tools.64.mono.exe --editor --path ./Example -client
