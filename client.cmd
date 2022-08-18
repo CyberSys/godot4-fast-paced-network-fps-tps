6a41cd6de86a2e2d3b9331089301242cef11ad6e
Stefan Boronczyk <stefan.amin.boronczyk@googlemail.com>
2022-08-18 22:01:23 +0200

    Delete client.cmd

--- client.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-:: --HAS ENDING BACKSLASH
-set batdir=%~dp0
-:: --MISSING ENDING BACKSLASH
-:: set batdir=%CD%
-pushd "%batdir%"
-dotnet msbuild ./Example/Shooter.sln  /restore /t:Build /p:Configuration=Debug /v:normal /p:GodotTargetPlatform=windows
-%batdir%\Editor\windows\godot.windows.opt.tools.64.mono.exe --path ./Example -client 
