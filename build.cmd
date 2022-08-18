008b1af5bf2b0f4251670836d643e14cb2a50f11
Stefan Boronczyk <stefan.amin.boronczyk@googlemail.com>
2022-08-18 22:01:26 +0200

    Delete build.cmd

--- build.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-:: --HAS ENDING BACKSLASH
-set batdir=%~dp0
-:: --MISSING ENDING BACKSLASH
-:: set batdir=%CD%
-pushd "%batdir%"
-dotnet msbuild ./Example/Shooter.sln /restore /t:Build /p:Configuration=Debug /v:normal /p:GodotTargetPlatform=windows
